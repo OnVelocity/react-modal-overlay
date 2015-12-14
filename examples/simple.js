8f7e40a269057787b871976a848a7f4d38428fe5
javadoug <doug@onvelocity.com>
2015-12-14 13:39:37 -0500

    pass children into foreground div

--- examples/simple.js
+++ examples/simple.js
@@ -3,7 +3,7 @@
 
 import React from 'react' //eslint-disable-line
 import ReactDOM from 'react-dom'
-import 'ov-react-modal-overlay/assets/index.less'
+//import 'ov-react-modal-overlay/assets/index.less'
 import ModalOverlay from 'ov-react-modal-overlay'
 
-ReactDOM.render(<ModalOverlay onClick={event => console.log(event)} />, document.getElementById('__react-content'))
+ReactDOM.render(<ModalOverlay onClick={event => console.log(event)}>{'This is the ModalOverlay component!'}</ModalOverlay>, document.getElementById('__react-content'))
--- src/ModalOverlay.jsx
+++ src/ModalOverlay.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import ClassNames from 'classnames'
+import 'ov-react-modal-overlay/assets/index.less'
 
 const ModalOverlay = props => {
 	const {className, otherProps} = props
@@ -9,10 +10,9 @@ const ModalOverlay = props => {
 		<div className={ClassNames('ov-react-modal-overlay', className)} {...otherProps} onClick={props.onClick}>
 			<div className='ov-react-modal-overlay-background'></div>
 			<div className='ov-react-modal-overlay-foreground'>
-				This is the ModalOverlay component!
+				{props.children}
 			</div>
 		</div>
-
 	)
 }
 
