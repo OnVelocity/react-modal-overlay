f5b107bf145e58ea1c5257f3e6fef50c9dadbe8c
javadoug <doug@onvelocity.com>
2015-12-13 23:39:39 -0500

    init

---
+++ examples/simple.js
@@ -0,0 +1,8 @@
+// use jsx to render html, do not modify simple.html
+
+import 'ov-react-modal-overlay/assets/index.less';
+import ModalOverlay from 'ov-react-modal-overlay';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<ModalOverlay />, document.getElementById('__react-content'));
---
+++ index.js
@@ -0,0 +1,3 @@
+// export this package's api
+import ModalOverlay from './src/';
+export default ModalOverlay;
